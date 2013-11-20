490fa7de2dd9ab864b29c4811d74483a86e62c0f
James Gorrie <jgorrie@gmail.com>
2013-11-20 09:01:04 +0000

    Update tests to work with Guardian deps

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 requirejs.config({
   paths: {
-    bean: '../lib/bean/bean',
-    bonzo: '../lib/bonzo/bonzo',
-    qwery: '../lib/qwery/qwery',
-    reqwest: '../lib/reqwest/reqwest',
-    component: '../component'
+    'bean': '../lib/bean/bean',
+    'bonzo': '../lib/bonzo/bonzo',
+    'utils/ajax': '../lib/qwery/qwery',
+    'reqwest': '../lib/reqwest/reqwest',
+    'component': '../component'
   },
   urlArgs: "bust=" +  (new Date()).getTime()
 });
